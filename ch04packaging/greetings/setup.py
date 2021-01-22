7f7e3e24bf7c2a791df635f173024cb96738a124
pittachalk <>
2021-01-22 17:07:28 +0000

    add hacky solution to get verison no

--- ch04packaging/greetings/setup.py
+++ ch04packaging/greetings/setup.py
@@ -1,12 +1,24 @@
 
 from setuptools import setup, find_packages
+import re
 
+# https://stackoverflow.com/questions/17791481/creating-a-version-attribute-for-python-packages-without-getting-into-troubl/41110107
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/__init__.py').read())
+    return result.group(1)
+
+project_name = 'greetings'
 setup(
     name = "Greetings",
-    version = "0.1.0",
+    #version = "0.1.0",
+    version = get_property('__version__', project_name),
     packages = find_packages(exclude=['*test']),
     install_requires = ['argparse'],
     entry_points={
         'console_scripts': [
             'greet = greetings.command:process'
-        ]})
+        ]}
+    )
+
+
+
